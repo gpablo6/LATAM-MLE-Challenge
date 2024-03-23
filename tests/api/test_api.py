357e27dd5a2c9d6a7c3c77c1e785d7b616104b1c
gpablo6 <gpablo6@outlook.com>
2024-03-23 04:07:37 +0000

    docs: update reports for api test

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -7,25 +7,25 @@ from challenge import app
 class TestBatchPipeline(unittest.TestCase):
     def setUp(self):
         self.client = TestClient(app)
-        
+
     def test_should_get_predict(self):
         data = {
             "flights": [
                 {
-                    "OPERA": "Aerolineas Argentinas", 
-                    "TIPOVUELO": "N", 
+                    "OPERA": "Aerolineas Argentinas",
+                    "TIPOVUELO": "N",
                     "MES": 3
                 }
             ]
         }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0])) # change this line to the model of chosing
+        # change this line to the model of chosing
+        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"predict": [0]})
-    
 
     def test_should_failed_unkown_column_1(self):
-        data = {       
+        data = { 
             "flights": [
                 {
                     "OPERA": "Aerolineas Argentinas", 
@@ -34,30 +34,32 @@ class TestBatchPipeline(unittest.TestCase):
                 }
             ]
         }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))# change this line to the model of chosing
+        # change this line to the model of chosing
+        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 400)
 
     def test_should_failed_unkown_column_2(self):
-        data = {        
+        data = {
             "flights": [
                 {
-                    "OPERA": "Aerolineas Argentinas", 
-                    "TIPOVUELO": "O", 
+                    "OPERA": "Aerolineas Argentinas",
+                    "TIPOVUELO": "O",
                     "MES": 13
                 }
             ]
         }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))# change this line to the model of chosing
+        # change this line to the model of chosing
+        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0]))
         response = self.client.post("/predict", json=data)
         self.assertEqual(response.status_code, 400)
-    
+
     def test_should_failed_unkown_column_3(self):
-        data = {        
+        data = {     
             "flights": [
                 {
-                    "OPERA": "Argentinas", 
-                    "TIPOVUELO": "O", 
+                    "OPERA": "Argentinas",
+                    "TIPOVUELO": "O",
                     "MES": 13
                 }
             ]
