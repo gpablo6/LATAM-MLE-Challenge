0715e5861c732da837857314e0a55962c3443455
gpablo6 <gpablo6@outlook.com>
2024-03-23 00:16:17 +0000

    docs: include initial report release

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -1,14 +1,16 @@
 import unittest
+from os import path
 import pandas as pd
 
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from challenge.model import DelayModel
 
+
 class TestModel(unittest.TestCase):
 
     FEATURES_COLS = [
-        "OPERA_Latin American Wings", 
+        "OPERA_Latin American Wings",
         "MES_7",
         "MES_10",
         "OPERA_Grupo LATAM",
@@ -24,12 +26,12 @@ class TestModel(unittest.TestCase):
         "delay"
     ]
 
-
     def setUp(self) -> None:
         super().setUp()
         self.model = DelayModel()
-        self.data = pd.read_csv(filepath_or_buffer="../data/data.csv")
-        
+        self.data = pd.read_csv(
+            filepath_or_buffer="./data/data.csv"
+        )
 
     def test_model_preprocess_for_training(
         self
@@ -47,7 +49,6 @@ class TestModel(unittest.TestCase):
         assert target.shape[1] == len(self.TARGET_COL)
         assert set(target.columns) == set(self.TARGET_COL)
 
-
     def test_model_preprocess_for_serving(
         self
     ):
@@ -59,7 +60,6 @@ class TestModel(unittest.TestCase):
         assert features.shape[1] == len(self.FEATURES_COLS)
         assert set(features.columns) == set(self.FEATURES_COLS)
 
-
     def test_model_fit(
         self
     ):
@@ -68,7 +68,9 @@ class TestModel(unittest.TestCase):
             target_column="delay"
         )
 
-        _, features_validation, _, target_validation = train_test_split(features, target, test_size = 0.33, random_state = 42)
+        _, features_validation, _, target_validation = train_test_split(
+            features, target, test_size=0.33, random_state=42
+        )
 
         self.model.fit(
             features=features,
@@ -79,13 +81,14 @@ class TestModel(unittest.TestCase):
             features_validation
         )
 
-        report = classification_report(target_validation, predicted_target, output_dict=True)
-        
+        report = classification_report(
+            target_validation, predicted_target, output_dict=True
+        )
+
         assert report["0"]["recall"] < 0.60
         assert report["0"]["f1-score"] < 0.70
         assert report["1"]["recall"] > 0.60
         assert report["1"]["f1-score"] > 0.30
-
 
     def test_model_predict(
         self
@@ -100,4 +103,7 @@ class TestModel(unittest.TestCase):
 
         assert isinstance(predicted_targets, list)
         assert len(predicted_targets) == features.shape[0]
-        assert all(isinstance(predicted_target, int) for predicted_target in predicted_targets)
+        assert all(
+            isinstance(predicted_target, int)
+            for predicted_target in predicted_targets
+        )
